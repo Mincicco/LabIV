4c5c88bf4c3d59a62494fb5e16b5b5bf348f4798
Mincicco <110043833+Mincicco@users.noreply.github.com>
2023-08-22 11:34:52 -0300

    Add files via upload

---
+++ TP1_LabIV/Controllers/PedidoYaBlueController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP1_LabIV.Service;
+
+namespace TP1_LabIV.Controllers
+{
+    public class PedidoYaBlueController
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class PedidosYaBlueController : ControllerBase
+        {
+            private readonly PedidoYaBlueService _service;
+
+            public PedidosYaBlueController(PedidoYaBlueService service)
+            {
+                _service = service;
+            }
+
+            [HttpPost("CreateOrder")]
+            public ActionResult<string> CreateNewOrder(string clientType)
+            {
+                return Ok(_service.CreateNewOrder(clientType));
+            }
+
+            [HttpGet("GetLocation")]
+            public ActionResult<string> GetLocation()
+            {
+                return Ok(_service.GetLocation());
+            }
+
+        }
+    }
+
+}
